40db3ea5474adf9d4377f4b5899b56c87abd3d8b
David Todd (c0de) <c0de@c0defox.es>
2018-07-06 02:08:39 -0500

    light cleanup

--- FIRMWARE/pixo-display/pixo-display-helper.ino
+++ FIRMWARE/pixo-display/pixo-display-helper.ino
@@ -17,19 +17,13 @@
 #include <ArduinoJson.h>
 #include "pixols.h"
 
-#define WIDTH        16 // Display width in pixels
-#define HEIGHT       16 // Display height in pixels
-#define MAX_FPS      45 
-
-#define MAX_X (WIDTH  * SCALE - 1) 
-#define MAX_Y (HEIGHT * SCALE - 1) 
-
 uint32_t        prevTime = 0;
 
 #define DATAPIN    19
 #define CLOCKPIN   18
 #define SHIFTDELAY 30
 #define BRIGHTNESS 15
+#define MAX_FPS    45 
 
 Adafruit_DotStarMatrix matrix = Adafruit_DotStarMatrix(
     16, 16, DATAPIN, CLOCKPIN,
